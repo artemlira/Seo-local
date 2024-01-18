1914dbdbb5c6f92050cfee7fa77898fd25ca3a30
Artem Lira <artemlira84@gmail.com>
2024-01-19 00:06:55 +0200

    add three section

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,9 @@
 import 'the-new-css-reset/css/reset.css';
 import '../styles/style.scss';
-import Swiper from 'swiper';
-import 'swiper/css';
 import burgerMenu from './burger-menu';
 // import tabs from "./tabs"; //Переключение табов
 // import './swiper'; // для подключения swiper
-import accordion from './accordion'; // для подключения accordion
+// import accordion from './accordion'; // для подключения accordion
 // import select from "./select"; //для подключения селекта
 // import "./phonemask"; //для подключения phonemask
 
@@ -15,29 +13,7 @@ burgerMenu(); // для бургер меню
 // tabs(".tabs__header", ".tabs__header-item", ".tabs__content-item", "active");
 
 // ==================== Переключение аккордиона ====================
-accordion();
+// accordion();
 
 // ==================== Переключение селекта ====================
 // select(); // для селекта
-
-const swiper = new Swiper('.services-swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
