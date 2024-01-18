0bb01fe84dec1a322539d136dc7449fb7b276f83
Artem <artemlira84@gmail.com>
2024-01-18 18:03:22 +0200

    create app

---
+++ src/js/accordion.js
@@ -0,0 +1,28 @@
+// Для подключения аккордиона нужно вставить следующую строку
+// <load ="partials/_accordion.html" title="Аккордион триггер" text="Какой-то текст для аккордеона"/>
+
+// Аккордеон
+export default function accordion() {
+  const items = document.querySelectorAll('.accordion__item');
+
+  const removeClasses = () => {
+    items.forEach((item) => {
+      item.classList.remove('active');
+    });
+  };
+
+  const addClass = (el) => {
+    el.classList.add('active');
+  };
+
+  items.forEach((item) => {
+    item.addEventListener('click', () => {
+      if (item.matches('.active')) {
+        removeClasses();
+      } else {
+        removeClasses();
+        addClass(item);
+      }
+    });
+  });
+}
